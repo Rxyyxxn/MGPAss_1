c8f269ba33030b4b302d82b0050f741208b17dd3
Rxyyxxn <rayyaanputra@gmail.com>
2022-11-21 12:22:20 +0800

    pausescrren

---
+++ MGP2022/app/src/main/java/com/example/mgp2022/Pausescreen.java
@@ -0,0 +1,105 @@
+package com.example.mgp2022;
+
+import android.app.Activity;
+import android.graphics.Canvas;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.content.Intent;
+
+// Created by TanSiewLan2021
+
+public class Pausescreen extends Activity implements OnClickListener, StateBase {  //Using StateBase class
+
+    //Define buttons
+    private Button btn_settings;
+    private Button btn_return;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Hide Title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Hide Top Bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.pausescreen);
+
+        btn_settings = (Button)findViewById(R.id.btn_settings);
+        btn_settings.setOnClickListener(this); //Set Listener to this button --> retry Button
+
+        btn_return = (Button)findViewById(R.id.btn_return);
+        btn_return.setOnClickListener(this); //Set Listener to this button --> quit Button
+
+        StateManager.Instance.AddState(new Deathscreen());
+    }
+
+    @Override
+    //Invoke a callback event in the view
+    public void onClick(View v)
+    {
+        // Intent = action to be performed.
+        // Intent is an object provides runtime binding.
+        // new instance of this object intent
+
+        Intent intent = new Intent();
+
+        if (v == btn_retry)
+        {
+            // intent --> to set to another class which another page or screen that we are launching.
+            intent.setClass(this, GamePage.class);
+            StateManager.Instance.ChangeState("MainGame"); // Default is like a loading page
+
+        }
+
+        else if (v == btn_quit)
+        {
+            intent.setClass(this, Mainmenu.class);
+        }
+        startActivity(intent);
+
+    }
+
+    @Override
+    public void Render(Canvas _canvas) {
+    }
+
+    @Override
+    public void OnEnter(SurfaceView _view) {
+    }
+
+    @Override
+    public void OnExit() {
+    }
+
+    @Override
+    public void Update(float _dt) {
+    }
+
+    @Override
+    public String GetName() {
+        return "Mainmenu";
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
